5c59b45217855766890268f7a076b2bf5497b7fd
jaly50 <jialic@andrew.cmu.edu>
2015-05-28 21:18:15 -0400

    whats the fuck

--- src/controller/FormAction.java
+++ src/controller/FormAction.java
@@ -19,13 +19,14 @@ public class FormAction extends Action{
 	@Override
 	public String perform(HttpServletRequest request) {
 		//return "form.jsp";
-		
+		/*
 		Set<String> set = new HashSet<String>();
 		set.add("forBusiness");
 		set.add("account balances");
 		HttpSession session= request.getSession();
 		session.setAttribute("what", set);
 		session.setAttribute("forBusiness", "true");
+		*/
 	
 		
 		return "readableForm.jsp";	
